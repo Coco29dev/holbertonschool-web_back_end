bf35c9439045cd6b7972401d5c6509737bab7c82
Coco29dev <10059@holbertonstudents.com>
2025-04-02 09:12:35 +0200

    with async/await

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,19 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const results = await Promise.allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ]);
 
-  return new Promise((resolve) => {
-    Promise.allSettled(promises).then((results) => {
-      const formattedResults = results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }));
-      resolve(formattedResults);
-    });
-  });
+    return results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }));
+  } catch (error) {
+    return [];
+  }
 }
