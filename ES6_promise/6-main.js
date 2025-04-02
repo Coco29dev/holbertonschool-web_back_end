52f2fc9d73b10cfa85460beb4de9f8bf5db73c22
Coco29dev <10059@holbertonstudents.com>
2025-04-02 09:40:43 +0200

    test

--- ES6_promise/6-main.js
+++ ES6_promise/6-main.js
@@ -1,8 +1,3 @@
 import handleProfileSignup from './6-final-user';
 
-const test = async () => {
-  const results = await handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg");
-  console.log(results);
-};
-
-test();
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
