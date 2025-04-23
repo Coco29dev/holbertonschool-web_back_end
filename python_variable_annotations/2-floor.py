c6c38ed3bb662bcc4ab3408f5c28864ca08e9e98
Coco29dev <10059@holbertonstudents.com>
2025-04-23 09:49:09 +0200

    basic annotations floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module pour des opérations mathématiques simples sur les flottants.
+"""
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Retourne la partie entière inférieure d’un nombre flottant.
+    """
+    return math.floor(n)
