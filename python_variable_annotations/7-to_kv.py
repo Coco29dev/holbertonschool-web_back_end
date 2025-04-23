453cc6342bc90e55296d303c7f9dd715c1d69694
Coco29dev <10059@holbertonstudents.com>
2025-04-23 11:19:29 +0200

    string and int/float to tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module de transformation de données typées en tuple.
+"""
+
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Retourne un tuple contenant une chaîne et
+    le carré d'un entier ou d'un flottant.
+    """
+    return (k, v * v)
