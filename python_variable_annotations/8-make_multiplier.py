58229740359ab69a314796c73207bdd419cf3518
Coco29dev <10059@holbertonstudents.com>
2025-04-23 11:43:34 +0200

    callable function

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module pour générer des fonctions de multiplication dynamiques.
+"""
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Retourne une fonction qui multiplie une
+    valeur float par le multiplicateur donné.
+    """
+
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
+
+    return multiplier_function
