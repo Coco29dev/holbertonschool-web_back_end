31c6e2cea54a241a89a754dc1d6ac7fec1948dc2
Coco29dev <10059@holbertonstudents.com>
2025-04-23 09:39:17 +0200

    basic annotations add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module simple pour effectuer des opérations mathématiques de base.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Retourne la somme de deux nombres flottants.
+    """
+    return a + b
