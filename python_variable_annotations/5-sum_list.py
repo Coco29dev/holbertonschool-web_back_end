25a25b1ff6ca154945dbe07443f7e9ddc5adbe8d
Coco29dev <10059@holbertonstudents.com>
2025-04-23 10:57:34 +0200

    type list of float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module pour effectuer des opérations sur des listes numériques.
+"""
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calcule et retourne la somme des éléments d'une liste de flottants.
+    """
+    return sum(input_list)
