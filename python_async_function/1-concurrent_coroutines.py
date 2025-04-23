03929e40149a0047523d17855dc39d6d6d2cd6fd
Coco29dev <10059@holbertonstudents.com>
2025-04-23 14:13:17 +0200

    execute multiple coroutine

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module asynchrone pour exécuter plusieurs délais aléatoires en parallèle.
+"""
+
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay) -> List[float]:
+    """
+    Lance n fois la coroutine wait_random avec max_delay et retourne
+    les délais dans l'ordre d'exécution.
+    """
+    ma_list = []
+    coroutine = [wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(coroutine):
+        resultat = await task
+        ma_list.append(resultat)
+    return ma_list
