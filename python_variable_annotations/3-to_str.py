ab3fb2e36b4bbd57594242df2ba58683c63012ea
Coco29dev <10059@holbertonstudents.com>
2025-04-23 09:58:25 +0200

    basic annnotation format

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module pour la conversion de types numériques en chaînes.
+"""
+
+def to_str(n: float) -> str:
+    """
+    Convertit un nombre flottant en chaîne de caractères.
+    """
+    return format(n)
