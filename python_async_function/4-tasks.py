d42bca73eab1e56b0bddc3f2fc95a4ff143b2f88
Coco29dev <10059@holbertonstudents.com>
2025-04-23 16:22:20 +0200

    task n2

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
+"""
+Module asynchrone pour exécuter plusieurs délais aléatoires en parallèle.
+"""
+
 
 import asyncio
+from typing import List
 
-task_wait_n = __import__('4-tasks').task_wait_n
+task_wait_random = __import__('3-tasks').task_wait_random
 
-n = 5
-max_delay = 6
-print(asyncio.run(task_wait_n(n, max_delay)))
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Lance n fois la coroutine wait_random avec max_delay et retourne
+    les délais dans l'ordre d'exécution.
+    """
+    ma_list = []
+    coroutine = [task_wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(coroutine):
+        resultat = await task
+        ma_list.append(resultat)
+    return ma_list
