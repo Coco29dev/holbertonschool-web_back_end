a80e434cd88c772a5a2340baf4e7f8cde1268454
Coco29dev <10059@holbertonstudents.com>
2025-04-23 15:09:28 +0200

    update type int

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Lance n fois la coroutine wait_random avec max_delay et retourne
     les délais dans l'ordre d'exécution.
--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Mesure le temps moyen d'attente pour exécuter wait_n n fois.
     """
