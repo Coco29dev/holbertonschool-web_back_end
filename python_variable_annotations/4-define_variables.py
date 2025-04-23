351d6fdf878057c9007e137cba17bbfee5da458f
Coco29dev <10059@holbertonstudents.com>
2025-04-23 10:49:41 +0200

    define type variable

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module de déclaration de variables avec des annotations de type.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
