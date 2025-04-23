977c411f4f09c6a1851837cbb98980cad055b7ff
Coco29dev <10059@holbertonstudents.com>
2025-04-23 09:42:28 +0200

    basic annotations str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module de manipulation de chaînes de caractères simples.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatène deux chaînes de caractères et retourne le résultat.
+    """
+    return str1 + str2
