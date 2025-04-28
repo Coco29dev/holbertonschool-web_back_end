d879a8c11810e3eebc8ebf5217c67c3bf3506911
Coco29dev <10059@holbertonstudents.com>
2025-04-28 10:54:47 +0200

    task async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
+"""
+Module pour récupérer des nombres flottants de manière asynchrone.
+"""
 
-import asyncio
-import random
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
+    """
+    Récupère 10 nombres flottants générés de façon asynchrone.
+
+    Retourne:
+        Liste de 10 flottants.
+    """
     result = [value async for value in async_generator() if value < 11]
     return result
