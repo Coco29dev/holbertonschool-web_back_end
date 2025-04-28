385fe44f10d1db1b8205983bd2c7d3a18e3ebe0c
Coco29dev <10059@holbertonstudents.com>
2025-04-28 13:42:47 +0200

    task run comprehensioné

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 
+"""
+Module de mesure de la durée d'exécution d'une coroutine.
+
+Ce module utilise asyncio pour exécuter des coroutines en parallèle
+et mesure le temps total nécessaire à leur exécution.
+"""
+
+
 import asyncio
+import time
+from typing import Awaitable
 
 
-measure_runtime = __import__('2-measure_runtime').measure_runtime
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def main():
-    return await(measure_runtime())
+async def measure_runtime() -> float:
+    """
+    Mesure et retourne le temps d'exécution pour lancer
+    async_comprehension en parallèle.
 
-print(
-    asyncio.run(main())
-)
+    Retourne :
+        float : Durée totale d'exécution en secondes.
+    """
+    depart = time.time()
+    await asyncio.gather(async_comprehension())
+    fin = time.time()
+    total = fin - depart
+    return total
