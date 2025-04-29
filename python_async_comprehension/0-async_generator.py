2e4e9346f6bcf268207c0cfcd22077f9ca3a1212
Coco29dev <10059@holbertonstudents.com>
2025-04-29 12:29:07 +0200

    update with generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@ Génère des nombres aléatoires de manière asynchrone.
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine asynchrone générant 10 nombres
     flottants aléatoires entre 0 et 10,
