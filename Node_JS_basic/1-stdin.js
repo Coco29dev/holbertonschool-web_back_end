0dd1978e859cd00ae49722a6a5324e320c18bd60
Coco29dev <10059@holbertonstudents.com>
2025-05-19 15:56:18 +0200

    update task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,14 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.setEncoding("utf-8");
+
+process.stdin.on("data", (input) => {
+  const name = input.trim();
+  console.log(`Your name is: ${name}`);
+
   if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
+    console.log("This important software is now closing");
   }
 
-  process.exit();
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
   process.exit();
 });
