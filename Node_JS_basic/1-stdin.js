ba1e71e1cabe01d27629f6b0501fdd4a0aabe207
Coco29dev <10059@holbertonstudents.com>
2025-05-19 15:32:48 +0200

    update task1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,15 @@ const { stdin: input, stdout: output } = require('node:process');
 
 const line = readline.createInterface({ input, output });
 
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
+
 line.question('Welcome to Holberton School, what is your name?\n', (réponse) => {
   console.log(`Your name is: ${réponse}`);
   line.close();
-  console.log('This important software is now closing');
+
+  if (process.stdin.isTTY) {
+    console.log('This important software ');
+  }
 });
