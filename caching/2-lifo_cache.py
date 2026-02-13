adef1f0794258c3d7fb7559d33c1fd20d5e96389
Coco29dev <10059@holbertonstudents.com>
2026-02-13 14:25:13 +0100

    add task 2  LIFO Caching

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache class
+    """
+
+    def put(self, key, item):
+        """ Assign to the dictionary self.cache_data the item value for the
+            key key.
+            If key or item is None, this method should not do anything.
+            If the number of items in self.cache_data is higher that
+            BaseCaching.MAX_ITEMS:
+                you must discard the last item put in cache (LIFO algorithm)
+                you must print DISCARD: with the key discarded and following
+                by a new line
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard = list(self.cache_data.keys())[-1]
+            print("DISCARD: {}".format(discard))
+            del self.cache_data[discard]
+        self.cache_data[key] = item
