8617012c6e45114a861da2a6580cc6332eb7bebd
Coco29dev <10059@holbertonstudents.com>
2026-02-23 10:26:32 +0100

    add task 2 create logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ Module that contains the function filter_datum
 """
 import re
 import logging
-from typing import List
+from typing import List, Tuple
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -33,3 +33,17 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super().format(record)
+
+
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "password")
+
+
+def get_logger() -> logging.Logger:
+    """Returns a logging.Logger configured for user data"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(handler)
+    return logger
