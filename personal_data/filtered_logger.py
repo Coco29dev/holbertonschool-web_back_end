49c55e32f0dae36f32533f3fd80607a769d57dc6
Coco29dev <10059@holbertonstudents.com>
2026-02-23 09:55:07 +0100

    update task 1 with func docu

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -29,6 +29,7 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ Filters values in incoming log records using filter_datum """
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super().format(record)
