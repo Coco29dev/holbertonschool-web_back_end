9f7063d5f237716b2129f2489b80c4f7e1b3b848
Coco29dev <10059@holbertonstudents.com>
2026-04-17 10:06:58 +0200

    add task 8

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -35,7 +35,8 @@ def before_request_handler() -> str:
                                             '/api/v1/forbidden/',
                                             '/api/v1/auth_session/login/']):
         return
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
     request.current_user = auth.current_user(request)
     if request.current_user is None:
