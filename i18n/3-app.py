0161e1c0a3575a1820b48180003c210fd52f6987
Coco29dev <10059@holbertonstudents.com>
2026-04-21 20:33:13 +0200

    add 3 paraterize templates

---
+++ i18n/3-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Basic Flask app."""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Render the index page."""
+    return render_template('3-index.html')
+
+
+def get_locale():
+    """Get the locale for the request."""
+    babel.init_app(app, locale_selector=get_locale)
+    return request.accept_languages.best_match(['de', 'fr', 'en'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
