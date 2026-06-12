7b3d13bdfaad647cff72e980f3f80100ff5cb1d0
Coco29dev <10059@holbertonstudents.com>
2026-06-12 10:21:58 +0200

    add task 1 in and not out

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create table useres
+-- id email name country 
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country enum('US', 'CO', 'TN') DEFAULT 'US', NOT NULL
+);
