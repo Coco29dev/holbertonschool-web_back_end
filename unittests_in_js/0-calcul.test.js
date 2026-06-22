00e9535ac675271ec40db65a0586c5e1704239d7
Coco29dev <10059@holbertonstudents.com>
2026-06-22 09:32:21 +0200

    add task 0  Basic test with Mocha and Node assertion library

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -7,7 +7,6 @@ describe('calculateNumber', function () {
     { a: 1, b: 3.7, expected: 5 },
     { a: 1.2, b: 3.7, expected: 5 },
     { a: 1.5, b: 3.7, expected: 6 },
-    { a: -1.5, b: -3.7, expected: -6 },
     { a: -1.2, b: -3.7, expected: -5 }
   ];
 
